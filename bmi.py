f9e6f5523ef507e88fd80635fde21fbe587fcb6b
Haziq <amirulhaziqba08.23@ichat.sp.edu.sg>
2024-10-29 14:37:51 +0800

    BMI

--- bmi.py
+++ bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(bmi))
     if bmi<18.5:
         print("You are Under Weight")
+        return -1
     elif bmi>25.0:
         print("You are Over Weight")
+        return 0
     else:
         print("You are Normal Weight")
+        return 1
 
 calculate_bmi(weight=57, height=1.73)
